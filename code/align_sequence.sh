488925ec9ccdc2a35c36e40e1963a3700331cf60
Jadesola AladeFa <aladefaj@gmail.com>
2021-08-05 18:23:09 -0500

    align rrnDB to SILVAs seed reference alignment, closes #4

---
+++ code/align_sequence.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+# author: Jadesola Alade-Fa
+# inputs: data/raw/rrnDB-5.7_16S_rRNA.fasta
+#         data/reference/silva.seed_v138_1.align
+#outputs: ata/raw/rrnDB-5.7_16S_rRNA.align
+#
+#We need to include flip=T to make sure all sequences are pointing in the same
+#direction.
+#
+
+code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.7_16S_rRNA.fasta, reference=data/references/silva_seed/silva.seed_v138_1.align, flip=T)'
