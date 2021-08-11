ae8c1bfa431b49b1614ba3a337f8f72031c874c6
Jadesola AladeFa <aladefaj@gmail.com>
2021-08-11 12:44:29 -0500

    rm spaces from sequence headers, closes #15

--- code/align_sequence.sh
+++ code/align_sequence.sh
@@ -9,4 +9,17 @@
 #direction.
 #
 
-code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.7_16S_rRNA.fasta, reference=data/references/silva_seed/silva.seed_v138_1.align, flip=T)'
+sed 's/ /_/g' data/raw/rrnDB-5.7_16S_rRNA.fasta > data/raw/rrnDB-5.7_16S_rRNA.temp.fasta
+
+
+code/mothur/mothur '#align.seqs(fasta=data/raw/rrnDB-5.7_16S_rRNA.temp.fasta,
+ reference=data/references/silva_seed/silva.seed_v138_1.align, flip=T)'
+
+if [[ $? -eq 0 ]]
+then
+  mv data/raw/rrnDB-5.7_16S_rRNA.temp.align data/raw/rrnDB-5.7_16S_rRNA.align
+  rm data/raw/rrnDB-5.7_16S_rRNA.temp.fasta
+else
+  echo "FAIL: Mothur failed to align sequences"
+  exit 1
+fi
